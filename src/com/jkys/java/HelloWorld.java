0895d29f9411ef2b77d528e904562caa2ed7f03e
李孟琨 <limengkun@limengkundeMacBook-Pro.local>
2020-07-28 15:39:24 +0800

    Initial commit

---
+++ src/com/jkys/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.jkys.java;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+
+    }
+
+}
